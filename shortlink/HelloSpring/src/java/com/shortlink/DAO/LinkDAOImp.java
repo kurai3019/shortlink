dc95b32eb06d0a4755b0d057a74c3d8628a799ab
Đạt <tuandat3019@gmail.com>
2018-10-13 00:50:58 +0700

    Update Code

--- shortlink/HelloSpring/src/java/com/shortlink/DAO/LinkDAOImp.java
+++ shortlink/HelloSpring/src/java/com/shortlink/DAO/LinkDAOImp.java
@@ -25,7 +25,7 @@ public class LinkDAOImp implements LinkDAO{
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             String url="jdbc:sqlserver://localhost:1433;databaseName=ShortLink";
             Connection con = DriverManager.getConnection(url, "sa", "123");
-            String sql = "insert into Link(Link_URL,Link_Code,Create_Date,Status,isCustomLink,Link_View,Create_User) values(?,?,GetDate(),1,0,0,?)";
+            String sql = "insert into Link(Link_URL,Link_Code,Create_Date,Status,Link_Type,Link_View,Create_User) values(?,?,GetDate(),1,0,0,?)";
             PreparedStatement stm = con.prepareStatement(sql);
             stm.setString(1, urla);
             stm.setString(2, randomkey);
